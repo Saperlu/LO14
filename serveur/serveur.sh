ec4996b277b208e965552fda2ef859971e3a03c2
Saperlu <saperli000@gmail.com>
2021-12-30 00:14:12 +0100

    client et serveur: mode browse ls et cd

--- serveur/serveur.sh
+++ serveur/serveur.sh
@@ -67,6 +67,55 @@ function interaction() {
 function wcl () {
     wc -l "$1" | egrep -o '[0-9]+' | head -n 1
 }
+
+function parseHeader () {
+    file="$1"
+    local line
+    read line < "$file"
+    local beginHeader=$(echo $line | cut -d: -f1)
+    local beginBody=$(echo $line | cut -d: -f2)
+    head -n $((beginBody-1)) "$file" | tail -n $((beginBody-beginHeader-1))
+}
+
+function listInDirectory () {
+    local file="$1"
+    local currentDir="$2"
+    parseHeader $file | awk "
+    BEGIN {state=0}
+    state==1 {state=2}
+    /^directory $(echo "$currentDir" | sed -r s/\\\\/\\\\\\\\/g)$/ {state=1}
+    state==2 && /^@$/ {state=3}
+    state==2 {print}"
+}
+
+function formatAbsolutePath () {
+    local currentDir="$1"
+    local destDir="$2"
+
+    # Formatage du destDir
+    if [[ ! "$destDir" =~ ^\\ ]]
+    then # Conversion de relatif vers absolu
+        destDir=$(printf %s\\\\%s "$currentDir" "$destDir")
+    fi
+    destDir=$(echo "$destDir" | sed -r 's/\\+/\\\\/g')
+    ## On remplace les \\.. par \\;
+    while [[ $(echo "$destDir" | egrep '\\\\\.\.(\\\\.*)?$') ]]
+    do
+        destDir=$(echo "$destDir" | sed -r 's/\\\\\.\.(\\\\.*)?$/\\\\;\1/g')
+    done
+    while [[ "$destDir" =~ \; ]]
+    do
+        destDir=$(echo "$destDir" | sed -r 's/\\\\[^\\;]+\\\\;/\\\\/')
+        destDir=$(echo "$destDir" | sed -r 's/\\+/\\\\/g')
+        if [[ "$destDir" =~ ^\\\\\; ]]
+        then
+            return 1 # Erreur
+        fi
+    done
+    echo "$destDir"
+    return 0
+}
+
 function commande-non-comprise () {
     echo "Le serveur ne peut pas interpréter cette commande"
 }
@@ -89,6 +138,7 @@ function commande-list () {
     sed "s:./\(.*\).archive.txt$:\1:g" find.txt
     rm find.txt
 }
+
 function commande-create () {
     nom=$1
     archive=$2
@@ -110,7 +160,8 @@ function commande-create () {
     find "$dossier" -type d | while read absolute
     do
         relative=$(printf "%s" $absolute | sed s:^$dossier::)
-        echo "directory $relative" >> header.txt
+        [[ "$relative" == '' ]] && relative="\\" # For root directory
+        echo "directory $relative\\" >> header.txt
         # On boucle sur le contenu du dossier pour afficher les informations de chaque fichier/dossier
         ls -l "$absolute" | sed /^total/d | while read rights _ _ _ size _ _ _ name
         do
@@ -170,26 +221,65 @@ function commande-create () {
 
 function commande-browse () {
     if [ "$(type -t $fun-$1)" = "function" ]; then
-        $fun-$1 $args
+        fichier="./$2.archive.txt"
+        if [[ -e "$fichier" ]]
+        then
+            $fun-$1 $args
+        else
+            echo "1
+            Erreur : l'archive n'existe pas"
+            exit 0
+        fi
     else
         commande-non-comprise $fun $args
     fi
 }
 
 function commande-browse-ls () {
-    echo "2
-            fichier -rwxrw-rw-
-            dossier drwxrwxrw-"
+    archive=$2
+    currentDir=$3
+    fichier="./$archive.archive.txt"
+    local list=$(listInDirectory "$fichier" "$currentDir")
+    local length=$(echo "$list" | wc -l | egrep -o [0-9]+)
+    if [[  $length -eq 0 ]]
+    then
+        echo "1
+        Le dossier est vide $length"
+    else
+        echo $length
+        echo "$list" | sed -r 's/(^.*) ([dlrwx-]+) ([0-9]*)( [0-9]+ [0-9]+)?$/\2 \1 \3/g'
+    fi
 }
 
 function commande-browse-cd () {
     archive=$2
     currentDir=$3
-    dossier=$4
+    destDir=$4
+    fichier="./$archive.archive.txt"
+
     # ici, il faut renvoyer le chemin absolu du nouveau répertoire de travail avec des / comme séparateur de dossier
     # chemin absolu = chemin relatif à la racine de l'archive
+    local destDir=$(formatAbsolutePath "$currentDir" "$destDir")
+    if [[ $? -eq 1 ]]
+    then
+        echo "1
+        Erreur : vous essayez de remonter au delà de la racine \\"
+        exit 0
+    fi
+    if [[ "$destDir" != "\\\\" ]]
+    then
+        local parentDir=$(echo "$destDir" | sed -r 's/^(.*\\\\)[^\\]+(\\\\)?$/\1/g' | sed -r 's/\\\\/\\/g')
+        local lastDir=$(echo "$destDir" | sed -r 's/\\\\([^\\]+)(\\\\)?$/\1/g')
+        local list=$(listInDirectory "$fichier" "$parentDir")
+        if [[ ! $(echo "$list" | egrep "^$lastDir d") ]]
+        then
+            echo "$(echo "$list" | wc -l)
+            Erreur : le dossier n'existe pas."
+        fi
+    fi
     echo "1
-            /$RANDOM"
+    $destDir"
+    
 }
 function commande-browse-cat () {
     archive=$2
@@ -198,6 +288,7 @@ function commande-browse-cat () {
     echo "1
             contenu du fichier"
 }
+
 function commande-browse-rm () {
     archive=$2
     currentDir=$3
@@ -205,6 +296,7 @@ function commande-browse-rm () {
     echo "1
             $2> $3 $4"
 }
+
 function commande-browse-touch () {
     archive=$2
     currentDir=$3
@@ -212,6 +304,7 @@ function commande-browse-touch () {
     echo "1
             $2> $3 $4"
 }
+
 function commande-browse-mkdir () {
     archive=$2
     currentDir=$3
